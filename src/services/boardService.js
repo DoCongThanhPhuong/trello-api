87df921dcad65552459c02c4ef1c848abb33341f
DoCongThanhPhuong <bido23082003@gmail.com>
2024-03-17 21:46:25 +0700

    API Create Board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
@@ -10,13 +11,17 @@ const createNew = async (reqBody) => {
     }
 
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong database
-    // ...
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
 
     // Làm thêm các xử lý logic khác với các Collection khác tùy thuộc vào đặc thù dự án...vv
     // Gửi Email, notification về cho Admin khi có board mới được tạo...vv
 
     // Trả kết quả về (TRONG SERVICE LUÔN PHẢI CÓ RETURN)
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
