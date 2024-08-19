b7e4d969599246e2d6a2ffd5334df586c280998b
DoCongThanhPhuong <bido23082003@gmail.com>
2024-08-19 14:08:47 +0700

    Update WHITELIST_DOMAINS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
-  'https://trello-web-black.vercel.app'
+  'https://trello-web-black.vercel.app',
+  'https://trello-api-fiiz.onrender.com'
 ]
 
 export const BOARD_TYPES = { PUBLIC: 'public', PRIVATE: 'private' }
